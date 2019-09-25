05b4c9993824217d155483cbe7d936328bbb51e4
NicoGangi5 <1605558@ucc.edu.ar>
2019-09-25 08:29:29 -0300

    fix tests

--- U03_Pilas/Ej-05/main.cpp
+++ U03_Pilas/Ej-05/main.cpp
@@ -3,5 +3,15 @@
 
 int main() {
     std::cout << "Ejercicio 03/05\n" << std::endl;
+
+    int word;
+    Pila<int> post;
+
+    char exp = '(6+2)∗5−8/4';
+
+    for (int i = 0; i < '\n'; ++i) {
+        if (exp[i] == )
+    }
+
     return 0;
 }
